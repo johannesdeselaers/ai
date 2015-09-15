623a09265de5c58431c97a1c40a7457a5e43f076
Diego A. Mancevo <dmancevo@gmail.com>
2015-09-15 11:12:28 +0200

    20

--- checkers_skeleton_cpp/player.cpp
+++ checkers_skeleton_cpp/player.cpp
@@ -8,196 +8,214 @@
 namespace checkers
 {
 
-	GameState Player::play(const GameState &pState, const Deadline &pDue)
-	{
-		//std::cerr << "Processing " << pState.toMessage() << std::endl;
-		std::string StateKey = pState.toMessage();
-		if (!nextStates.count(StateKey)) pState.findPossibleMoves(nextStates[StateKey]);
+    GameState Player::play(const GameState &pState, const Deadline &pDue)
+    {
+        //std::cerr << "Processing " << pState.toMessage() << std::endl;
+        std::string StateKey = pState.toMessage();
+        if (!nextStates.count(StateKey)) pState.findPossibleMoves(nextStates[StateKey]);
 
-		//if (lNextStates.size() == 0) return GameState(pState, Move());
-		if (nextStates[StateKey].size() == 0) return GameState(pState, Move());
+        //if (lNextStates.size() == 0) return GameState(pState, Move());
+        if (nextStates[StateKey].size() == 0) return GameState(pState, Move());
 
-		/*
-		* Here you should write your clever algorithms to get the best next move, ie the best
-		* next state. This skeleton returns a random move instead.
-		*/
+        /*
+        * Here you should write your clever algorithms to get the best next move, ie the best
+        * next state. This skeleton returns a random move instead.
+        */
 
-		//Determine player's color.
-		uint8_t nextPlayer = pState.getNextPlayer();
-		if (nextPlayer&CELL_RED) color = 1;
-		else if (nextPlayer&CELL_WHITE) color = -1;
+        //Determine player's color.
+        uint8_t nextPlayer = pState.getNextPlayer();
+        if (nextPlayer&CELL_RED) color = 1;
+        else if (nextPlayer&CELL_WHITE) color = -1;
 
-		//Initialize move choice.
-		unsigned int move;
+        //Initialize move choice.
+        unsigned int move;
 
-		//Must move in less than one second.
-		double time_left_before = 1.0;
+        //Must move in less than one second.
+        double time_left_before = 1.0;
 
-		//Iterative deepening
-		for (int d = 1; d < 9; d++)
-		{
-			//Initialize value to minus infinity.
-			double value = -1 * std::numeric_limits<double>::infinity();
+        //Iterative deepening
+        for (int d = 1; d < 9; d++)
+        {
+            //Initialize value to minus infinity.
+            double value = -1 * std::numeric_limits<double>::infinity();
 
-			//Initialize move to the first given.
-			move = 0;
+            //Initialize move to the first given.
+            move = 0;
 
-			//Initialize alpha and beta values to minus and plus infinity respectively.
-			double alpha = -1 * std::numeric_limits<double>::infinity();
-			double beta = std::numeric_limits<double>::infinity();
+            //Initialize alpha and beta values to minus and plus infinity respectively.
+            double alpha = -1 * std::numeric_limits<double>::infinity();
+            double beta = std::numeric_limits<double>::infinity();
 
-			//Get the current time at this point.
-			std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+            //Get the current time at this point.
+            std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-			for (unsigned int m = 0; m < nextStates[StateKey].size(); m++)
-			{
-				//Select move if its a win.
-				if ((color == 1 && pState.isRedWin()) || (color == -1 && pState.isWhiteWin()))
-					return nextStates[StateKey][m];
+            for (unsigned int m = 0; m < nextStates[StateKey].size(); m++)
+            {
+                //Select move if its a win.
+                if ((color == 1 && pState.isRedWin()) || (color == -1 && pState.isWhiteWin()))
+                    return nextStates[StateKey][m];
 
-				//Otherwise compute the value of the child state.
-				float child_value = Player::MiniMaxAB(nextStates[StateKey][m], d, alpha, beta, false);
+                //Otherwise compute the value of the child state.
+                float child_value = Player::MiniMaxAB(nextStates[StateKey][m], d, alpha, beta, false);
 
-				//Remember the best move so far.
-				if (child_value > value)
-				{
-					value = child_value;
-					move = m;
-				}
-			}
+                //Remember the best move so far.
+                if (child_value > value)
+                {
+                    value = child_value;
+                    move = m;
+                }
+            }
 
-			//Get the time at this point.
-			std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+            //Get the time at this point.
+            std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
-			//Compute the amount of time taken in the previous iteration.
-			std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-			
-			//Compute the amounf of time left at this point.
-			double time_left = time_left_before - time_span.count();
-			
-			//Return move if there is not enough time for the next iteration.
-			if (6 * (time_left_before - time_left) > time_left) return nextStates[StateKey][move];
-			
-			//Update the amount of time left before we perform another search (one level deeper).
-			time_left_before -= time_span.count();
+            //Compute the amount of time taken in the previous iteration.
+            std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            
+            //Compute the amounf of time left at this point.
+            double time_left = time_left_before - time_span.count();
+            
+            //Return move if there is not enough time for the next iteration.
+            if (6 * (time_left_before - time_left) > time_left) return nextStates[StateKey][move];
+            
+            //Update the amount of time left before we perform another search (one level deeper).
+            time_left_before -= time_span.count();
 
-		}
+        }
 
-		//In theory we should never reach this line but without it the compiler complains.
-		return nextStates[StateKey][move];
-	}
+        //In theory we should never reach this line but without it the compiler complains.
+        return nextStates[StateKey][move];
+    }
 
-	double Player::MiniMaxAB(const GameState &pState, int depth, double alpha, double beta, bool maxPlayer)
-	{
-		//Node StateKey
-		std::string StateKey = pState.toMessage();
+    double Player::MiniMaxAB(const GameState &pState, int depth, double alpha, double beta, bool maxPlayer)
+    {
+        //Node StateKey
+        std::string StateKey = pState.toMessage();
+        
+        //Return the static game value if we're at a leaf node.
+        if (!depth)
+        {
+            if (!gameValues.count(StateKey)) Player::StaticGameValue(pState, StateKey);
+            return gameValues[StateKey];
+        }
+        else if (pState.isDraw()) return 0.0;
+        else if (pState.isRedWin())
+        {
+            if (color == 1) return 10000.0;
+            else return -10000.0;
+        }
+        else if (pState.isWhiteWin())
+        {
+            if (color == -1) return 10000.0;
+            else return -10000.0;
+        }
+        //Otherwise compute the child nodes and compute their values recursively.
+        else
+        {
+            //Get GameState children (possible next moves).
+            if (!nextStates.count(StateKey))
+                pState.findPossibleMoves(nextStates[StateKey]);
+                
+                
+            if (maxPlayer)
+            {
+                //Initialize value to minus infinity.
+                double value = -1 * std::numeric_limits<double>::infinity();
+                
+                //Iterate through the state's next possible games.
+                for (unsigned int i=0; i < nextStates[StateKey].size(); i++)
+                {
+                    //Increase depth by one when whe there is only one possible move (explored beyond forzed moves).
+                    double next_depth;
+                    if (nextStates[StateKey].size() == 1) next_depth = (depth);
+                    else next_depth = (depth - 1);
+                    
+                    //Get child value
+                    double child_value = Player::MiniMaxAB(nextStates[StateKey][i], next_depth, alpha, beta, false);
+                    
+                    //Update value and alpha.
+                    value = std::max(value, child_value);
+                    alpha = std::max(value, alpha);
 
-		if (!depth)
-		{
-			if (!gameValues.count(StateKey)) Player::StaticGameValue(pState, StateKey);
-			return gameValues[StateKey];
-		}
-		else if (pState.isDraw()) return 0.0;
-		else if (pState.isRedWin())
-		{
-			if (color == 1) return 10000.0;
-			else return -10000.0;
-		}
-		else if (pState.isWhiteWin())
-		{
-			if (color == -1) return 10000.0;
-			else return -10000.0;
-		}
-		else
-		{
-			//Get GameState children (possible next moves).
-			if (!nextStates.count(StateKey))
-				pState.findPossibleMoves(nextStates[StateKey]);
+                    //Beta cut-off
+                    if (beta <= alpha) break;
+                }
+                
+                return value;
+            }
+            else
+            {
+               //Initialize value to plus infinity.
+                double value = 1 * std::numeric_limits<double>::infinity();
+                
+                //Iterate through the state's next possible games.
+                for (unsigned int i=0; i < nextStates[StateKey].size(); i++)
+                {
+                    //Increase depth by one when whe there is only one possible move (explored beyond forzed moves).
+                    double next_depth;
+                    if (nextStates[StateKey].size() == 1) next_depth = (depth);
+                    else next_depth = (depth - 1);
+                    
+                    //Get child value
+                    double child_value = Player::MiniMaxAB(nextStates[StateKey][i], next_depth, alpha, beta, true);
+                    
+                    //Update value and beta.
+                    value = std::min(value, child_value);
+                    beta = std::min(value, beta);
 
-			//Initialize value to minus/plus infinity.
-			double value;
-			if (maxPlayer) value = -1 * std::numeric_limits<double>::infinity();
-			else value = std::numeric_limits<double>::infinity();
+                    //Alpha cut-off
+                    if (beta <= alpha) break;
+                }
+                
+                return value; 
+            }
+        }
+    }
 
-			for (unsigned int i = 0; i < nextStates[StateKey].size(); i++)
-			{
-				//Increase depth by one when whe there is only one possible move (explored beyond forzed moves).
-				double next_depth;
-				if (nextStates[StateKey].size() == 1) next_depth = (depth);
-				else next_depth = (depth - 1);
+    void Player::StaticGameValue(const GameState &pState, const std::string StateKey)
+    {
+        //Points awarded for regular pieces (zero-zum).
+        //Points for regular pieces stored at index 0.
+        //Points for king pieces stored at index 1.
+        int materialPoints[2] = { 0, 0 };
+        Player::materialValue(pState, materialPoints);
 
-				//Get child value
-				double child_value = Player::MiniMaxAB(nextStates[StateKey][i], next_depth, alpha, beta, !maxPlayer);
+        //Moves left until draw.
+        int movesLeft = (int)pState.getMovesUntilDraw();
 
-				if (maxPlayer)
-				{
-					//Update value and alpha.
-					value = std::max(value, child_value);
-					alpha = std::max(value, alpha);
+        //Available moves.
+        if (!nextStates.count(StateKey)) pState.findPossibleMoves(nextStates[StateKey]);
+        int availableMoves = nextStates[StateKey].size();
 
-					//Beta cut-off
-					if (beta <= alpha) break;
-				}
-				else
-				{
-					//Update value and beta.
-					value = std::min(value, child_value);
-					beta = std::min(value, beta);
+        //Heuristic (linear polynomial).
+        gameValues[StateKey] = B1 * color * materialPoints[0] + B2 * color * materialPoints[1] + B3 * availableMoves + (B4 / movesLeft);
+    }
 
-					//Alpha cut-off
-					if (beta <= alpha) break;
-				}
+    void Player::materialValue(const GameState &pState, int materialPoints[])
+    {
 
-			}
+        //There are 31 cells in the board.
+        for (int i = 0; i < 32; i++)
+        {
 
-			return value;
-		}
-	}
+            //Points awarded depending on the piece position on the board.
+            int points = positionPoints[i];
 
-	void Player::StaticGameValue(const GameState &pState, const std::string StateKey)
-	{
-		//Points awarded for regular pieces (zero-zum).
-		//Points for regular pieces stored at index 0.
-		//Points for king pieces stored at index 1.
-		int materialPoints[2] = { 0, 0 };
-		Player::materialValue(pState, materialPoints);
+            //If the cell is occupied by white, increment points.
+            if (pState.at(i)&CELL_RED)
+            {
+                if (pState.at(i)&CELL_KING) materialPoints[1] += points;
+                else materialPoints[0] += points;
+            }
+            //If the piece belongs to the oponent, substract points as appropiate.
+            else if (pState.at(i)&CELL_WHITE)
+            {
+                if (pState.at(i)&CELL_KING) materialPoints[1] -= points;
+                else materialPoints[0] -= points;
+            }
+        }
 
-		//Moves left until draw.
-		int movesLeft = (int)pState.getMovesUntilDraw();
+    }
 
-		//Available moves.
-		if (!nextStates.count(StateKey)) pState.findPossibleMoves(nextStates[StateKey]);
-		int availableMoves = nextStates[StateKey].size();
-
-		//Heuristic (linear polynomial).
-		gameValues[StateKey] = B1 * color * materialPoints[0] + B2 * color * materialPoints[1] + B3 * availableMoves + (B4 / movesLeft);
-	}
-
-	void Player::materialValue(const GameState &pState, int materialPoints[])
-	{
-
-		//There are 31 cells in the board.
-		for (int i = 0; i < 32; i++)
-		{
-
-			//Points awarded depending on the piece position on the board.
-			int points = positionPoints[i];
-
-			//If the cell is occupied by white, increment points.
-			if (pState.at(i)&CELL_RED)
-			{
-				if (pState.at(i)&CELL_KING) materialPoints[1] += points;
-				else materialPoints[0] += points;
-			}
-			//If the piece belongs to the oponent, substract points as appropiate.
-			else if (pState.at(i)&CELL_WHITE)
-			{
-				if (pState.at(i)&CELL_KING) materialPoints[1] -= points;
-				else materialPoints[0] -= points;
-			}
-		}
-
-	}
-
-	/*namespace checkers*/
+    /*namespace checkers*/
 }
--- checkers_skeleton_cpp/player.hpp
+++ checkers_skeleton_cpp/player.hpp
@@ -11,66 +11,66 @@
 namespace checkers
 {
 
-	class Player
-	{
-	public:
-		///perform a move
-		///\param pState the current state of the board
-		///\param pDue time before which we must have returned
-		///\return the next state the board is in after our move
-		GameState play(const GameState &pState, const Deadline &pDue);
+    class Player
+    {
+    public:
+        ///perform a move
+        ///\param pState the current state of the board
+        ///\param pDue time before which we must have returned
+        ///\return the next state the board is in after our move
+        GameState play(const GameState &pState, const Deadline &pDue);
 
-		//Player's color (1 for red, -1 for white).
-		int color;
+        //Player's color (1 for red, -1 for white).
+        int color;
 
-		//Scoring parameters
-		const double B1 = 1.0; //Pawn pieces
-		const double B2 = 2.0; //King pieces
-		const double B3 = 1.0; //Available moves
-		const double B4 = -5.0; //Moves until draw
+        //Scoring parameters
+        const double B1 = 1.0; //Pawn pieces
+        const double B2 = 2.0; //King pieces
+        const double B3 = 1.0; //Available moves
+        const double B4 = -5.0; //Moves until draw
 
-		//Points awarded depending on the piece position on the board.
-		//http://ai-depot.com/articles/minimax-explained/3/
-		std::unordered_map<int, int> positionPoints;
+        //Points awarded depending on the piece position on the board.
+        //http://ai-depot.com/articles/minimax-explained/3/
+        std::unordered_map<int, int> positionPoints;
 
-		//Some hash maps to speed things up a bit.
-		std::unordered_map<std::string, std::vector<GameState>> nextStates;
-		std::unordered_map<std::string, double> gameValues;
+        //Some hash maps to speed things up a bit.
+        std::unordered_map<std::string, std::vector<GameState>> nextStates;
+        std::unordered_map<std::string, double> gameValues;
 
-		//Constructor
-		Player()
-		{
-			//Assign values to board positions.
-			for (int i = 0; i < 32; i++)
-			{
-				if (i < 5) positionPoints[i] = 4;
-				else if (i < 9) positionPoints[i] = 3;
-				else if (i < 9) positionPoints[i] = 3;
-				else if (i < 11) positionPoints[i] = 2;
-				else if (i < 13) positionPoints[i] = 4;
-				else if (i < 14) positionPoints[i] = 2;
-				else if (i < 15) positionPoints[i] = 1;
-				else if (i < 17) positionPoints[i] = 1;
-				else if (i < 18) positionPoints[i] = 3;
-				else if (i < 19) positionPoints[i] = 2;
-				else if (i < 21) positionPoints[i] = 4;
-				else if (i < 23) positionPoints[i] = 2;
-				else if (i < 27) positionPoints[i] = 3;
-				else positionPoints[i] = 4;
-			}
-		}
+        //Constructor
+        Player()
+        {
+            //Assign values to board positions.
+            for (int i = 0; i < 32; i++)
+            {
+                if (i < 5) positionPoints[i] = 4;
+                else if (i < 9) positionPoints[i] = 3;
+                else if (i < 9) positionPoints[i] = 3;
+                else if (i < 11) positionPoints[i] = 2;
+                else if (i < 13) positionPoints[i] = 4;
+                else if (i < 14) positionPoints[i] = 2;
+                else if (i < 15) positionPoints[i] = 1;
+                else if (i < 17) positionPoints[i] = 1;
+                else if (i < 18) positionPoints[i] = 3;
+                else if (i < 19) positionPoints[i] = 2;
+                else if (i < 21) positionPoints[i] = 4;
+                else if (i < 23) positionPoints[i] = 2;
+                else if (i < 27) positionPoints[i] = 3;
+                else positionPoints[i] = 4;
+            }
+        }
 
-		//Scoring function
-		void StaticGameValue(const GameState &pState, const std::string StateKey);
+        //Scoring function
+        void StaticGameValue(const GameState &pState, const std::string StateKey);
 
-		//Points awarded for material (zero-sum).
-		void materialValue(const GameState &pState, int materialPoints[]);
+        //Points awarded for material (zero-sum).
+        void materialValue(const GameState &pState, int materialPoints[]);
 
-		//MiniMax algorithm with Alpha Beta pruning.
-		double MiniMaxAB(const GameState &pState, int depth, double alpha, double beta, bool maxPlayer);
-	};
+        //MiniMax algorithm with Alpha Beta pruning.
+        double MiniMaxAB(const GameState &pState, int depth, double alpha, double beta, bool maxPlayer);
+    };
 
-	/*namespace checkers*/
+    /*namespace checkers*/
 }
 
 #endif
